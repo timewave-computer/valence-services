9e075a1056f68e3bf37979951d32adff048d5bdb
Art3mix <r3mix.il@gmail.com>
2023-11-16 10:22:14 +0200

    Audit issue 8 - whitelist complexity fix

--- contracts/services_manager/src/state.rs
+++ contracts/services_manager/src/state.rs
@@ -1,7 +1,9 @@
+use std::collections::HashSet;
+
 use cosmwasm_std::Addr;
 use cw_storage_plus::{Item, Map};
 
 pub(crate) const SERVICES_TO_ADDR: Map<String, Addr> = Map::new("services_to_addr");
 pub(crate) const ADDR_TO_SERVICES: Map<Addr, String> = Map::new("addr_to_services");
 
-pub(crate) const WHITELISTED_CODE_IDS: Item<Vec<u64>> = Item::new("wl_code_ids");
+pub(crate) const ACCOUNT_WHITELISTED_CODE_IDS: Item<HashSet<u64>> = Item::new("wl_code_ids");
